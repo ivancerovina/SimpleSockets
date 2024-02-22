735680fdc3ef6f9c3a477fe0c7e12ee452050da2
Ivan Cerovina <ivancerovina@gmail.com>
2024-02-22 10:04:02 +0100

    jitpack

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,13 +7,15 @@ version = "1.0-SNAPSHOT"
 
 repositories {
     mavenCentral()
+    maven("https://jitpack.io")
 }
 
 dependencies {
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
-    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1");
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.16.1")
     implementation("org.reflections:reflections:0.10.2")
+    implementation("com.github.jitpack:gradle-simple:1.0");
 }
 
 tasks.test {
